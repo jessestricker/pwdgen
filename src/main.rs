2caea5f19cf9217567368115529e527fa00081e9
Jesse Stricker <git@jessestricker.de>
2022-11-03 19:13:22 +0100

    feat: add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::num::NonZeroU32;
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(version, author, about)]
+struct Args {
+    /// The length of the generated passwords.
+    #[arg(short, long, default_value = "16")]
+    length: NonZeroU32,
+
+    /// The number of passwords to generate.
+    #[arg(short, long, default_value = "1")]
+    count: NonZeroU32,
+
+    /// The sets of character to sample from.
+    #[arg(short, long, value_enum, default_values_t = Args::DEFAULT_TYPES)]
+    types: Vec<NamedCharSet>,
+
+    /// Print extra information about the generated passwords.
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+impl Args {
+    const DEFAULT_TYPES: [NamedCharSet; 4] = [
+        NamedCharSet::Uppercase,
+        NamedCharSet::Lowercase,
+        NamedCharSet::Digits,
+        NamedCharSet::Symbols,
+    ];
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+pub enum NamedCharSet {
+    Uppercase,
+    Lowercase,
+    Digits,
+    Symbols,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(&args);
 }

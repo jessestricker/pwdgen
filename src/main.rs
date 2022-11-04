397659a3b9e1f4f2f4942e0c4daaa20fbdcd2d8e
Jesse Stricker <git@jessestricker.de>
2022-11-04 14:05:04 +0100

    feat: generate passwords with CLI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::num::NonZeroU32;
+use std::{collections::BTreeSet, num::NonZeroU32};
 
 use clap::{Parser, ValueEnum};
+use pwdgen::{CharSet, Generator, PasswordSpec};
+use rand::rngs::OsRng;
 
 #[derive(Parser, Debug)]
 #[command(version, author, about)]
@@ -29,17 +31,51 @@ impl Args {
         NamedCharSet::Digits,
         NamedCharSet::Symbols,
     ];
+
+    fn to_password_spec(&self) -> PasswordSpec {
+        let types_set = BTreeSet::from_iter(self.types.iter().cloned());
+        let char_sets = types_set.into_iter().map(|ncs| ncs.to_char_set()).collect();
+        PasswordSpec::new(self.length, char_sets)
+    }
 }
 
-#[derive(ValueEnum, Clone, Debug)]
-pub enum NamedCharSet {
+#[derive(ValueEnum, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+enum NamedCharSet {
     Uppercase,
     Lowercase,
     Digits,
     Symbols,
 }
 
+impl NamedCharSet {
+    fn to_char_set(&self) -> CharSet {
+        let predicate = match self {
+            NamedCharSet::Uppercase => char::is_ascii_uppercase,
+            NamedCharSet::Lowercase => char::is_ascii_lowercase,
+            NamedCharSet::Digits => char::is_ascii_digit,
+            NamedCharSet::Symbols => char::is_ascii_punctuation,
+        };
+        ('\x00'..='\x7f').filter(predicate).collect()
+    }
+}
+
 fn main() {
     let args = Args::parse();
-    dbg!(&args);
+    let spec = args.to_password_spec();
+
+    if args.verbose {
+        eprintln!("Length:     {}", args.length);
+        eprintln!("Count:      {}", args.count);
+        eprintln!("Entropy:    {:.2}", spec.entropy());
+        eprintln!(
+            "Characters: ({}) {}",
+            spec.char_set_union().len(),
+            spec.char_set_union().iter().collect::<String>()
+        );
+        eprintln!();
+    }
+
+    let gen = Generator::new(OsRng, spec);
+    gen.take(args.count.get() as usize)
+        .for_each(|pwd| println!("{}", pwd));
 }
